6f97d2d28db436338c0dba2f0f29c6d8e963ac95
kimcipher <kimutaij192@gmail.com>
2022-10-17 12:01:27 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,49 @@
+# client server
+# framework
+# flask
+# request-response
+
+# client -> computer that is listens to server response & sends requests to server.(web browser)
+# server -> processes client request then sends respond to the client. (flask app)
+# framework -> make development easy by utilizing the reusable code...flask, django
+
+
+from flask import Flask, render_template, sessions, request
+
+# start
+app = Flask(__name__)
+
+
+# decorators(@)
+# flask routing
+# method -> route()
+# to  bind the method route to your app object, then we use decorator @
+
+@app.route('/home')
+def home_page():
+    return "Hello welcome to flask"
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def sign_up():
+    return render_template('register.html')
+
+
+@app.route('/')
+def index():
+    return "view the index page"
+
+
+@app.route('/add')
+def add():
+    fruits = ['orange','pineapple','apple']
+    return fruits
+
+
+app.run(debug=True)
+# end
