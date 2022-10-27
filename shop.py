d0ae3693d3d93330087c5e101bc813e0daa75b4b
kimcipher <kimutaij192@gmail.com>
2022-10-27 05:44:12 +0300

    third commit

--- shop.py
+++ shop.py
@@ -40,6 +40,20 @@ def products():
         return redirect('/login')
 
 
+@app.route('/m')
+def medicine():
+    if 'key' in session:
+
+        cursor_medicine = connection.cursor()
+        sql_medicine = 'SELECT * FROM medicine'
+        cursor_medicine.execute(sql_medicine)
+        medicine = cursor_medicine.fetchall()
+
+        return render_template('medicine.html', medicine=medicine)
+    else:
+        return redirect('/login')
+
+
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
@@ -97,6 +111,85 @@ def single(product_id):
         return redirect('/login')
 
 
+@app.route('/single_item/<medicine_id>')
+def single_item(medicine_id):
+    if 'key' in session:
+        cursor = connection.cursor()
+        sql = 'SELECT * FROM medicine WHERE medicine_id=%s'
+        cursor.execute(sql, medicine_id)
+        row = cursor.fetchone()
+        return render_template('single_item.html', item=row)
+    else:
+        return redirect('/login')
+
+
+# modcom.co.ke/sql/payment
+import requests
+import base64
+import datetime
+
+from requests.auth import HTTPBasicAuth
+
+
+@app.route('/mpesa_payment', methods=['POST', 'GET'])
+def mpesa():
+    if request.method == 'POST':
+        phone = request.form['phone']
+        amount = request.form['amount']
+
+        consumer_key = "GTWADFxIpUfDoNikNGqq1C3023evM6UH"
+        consumer_secret = "amFbAoUByPV2rM5A"
+
+        api_url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
+        r = requests.get(api_url, auth=HTTPBasicAuth(consumer_key, consumer_secret))
+
+        data = r.json()
+
+        access_token = "Bearer" + ' ' + data['access_token']
+
+        #  GETTING THE PASSWORD
+        timestamp = datetime.datetime.today().strftime('%Y%m%d%H%M%S')
+
+        passkey = 'bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919'
+        business_short_code = "174379"
+
+        data = business_short_code + passkey + timestamp
+        encoded = base64.b64encode(data.encode())
+
+        password = encoded.decode('utf-8')
+
+        # BODY OR PAYLOAD
+        payload = {
+            "BusinessShortCode": "174379",
+            "Password": "{}".format(password),
+            "Timestamp": "{}".format(timestamp),
+            "TransactionType": "CustomerPayBillOnline",
+            "Amount": amount,  # use 1 when testing
+            "PartyA": phone,  # change to your number
+            "PartyB": "174379",
+            "PhoneNumber": phone,
+            "CallBackURL": "https://modcom.co.ke/job/confirmation.php",
+            "AccountReference": "Modcom",
+            "TransactionDesc": "Modcom"
+        }
+
+        # POPULATING THE HTTP HEADER
+        headers = {
+            "Authorization": access_token,
+            "Content-Type": "application/json"
+        }
+
+        url = "https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest"
+
+        response = requests.post(url, json=payload, headers=headers)
+        print(response.text)
+
+        return render_template('mpesa_payment.html', message="Please check your phone for Payment")
+
+    else:
+        return render_template('mpesa_payment.html')
+
+
 app.run(debug=True)
 # end app
 
@@ -104,15 +197,6 @@ app.run(debug=True)
 # variables {{variable}}
 # {% python statements%}, if conditions, for loops
 
-# {% for item in mydata%}
-# {% endfor %}
-
-# sessions
-
-# {% for item in mydata%}
-# {% endfor %}
-
-# sessions
 # {% for item in mydata%}
 # {% endfor %}
 
